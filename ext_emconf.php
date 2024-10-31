c9eab86e3e87c3056e80ccf8f8d6d54808071e0b
Kris <Kristian@passionweb.de>
2024-10-31 17:43:05 +0100

    [TASK] init custom extension codebreak

---
+++ ext_emconf.php
@@ -0,0 +1,19 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'Custom extension development',
+    'description' => 'Shows the custom development of extension in TYPO3 and their customization options',
+    'category' => 'be',
+    'author' => 'Manuel Schnabel',
+    'author_email' => 'service@passionweb.de',
+    'author_company' => 'PassionWeb Manuel Schnabel',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '13.0.0-13.4.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
